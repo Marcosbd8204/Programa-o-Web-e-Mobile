d50ae9f95c4fec13f7d085ff1a5efa57a09bd157
Marcos Vinicius <83377302+Marcosbd8204@users.noreply.github.com>
2026-04-13 12:48:21 +0000

    +1 nessa desgraça

--- Projeto GQ1/js/script.js
+++ Projeto GQ1/js/script.js
@@ -15,4 +15,4 @@ botaoTema.addEventListener("click", () => {
 const ano = document.getElementById("ano");
 const anoAtual = new Date().getFullYear();
 
-ano.textContent = `© ${anoAtual}`;
+ano.textContent = `© ${anoAtual}`;  
